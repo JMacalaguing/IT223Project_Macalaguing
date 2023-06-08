0162cb68517ae754771b95802572876b990d301a
JMacalaguing <125894320+JMacalaguing@users.noreply.github.com>
2023-06-08 21:36:39 +0800

    Updated version

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 function myFunction() {
     document.getElementById("navbarNav").classList.toggle("show");
   }
-
-  // Close the dropdown if the user clicks outside of it
   window.onclick = function(event) {
     if (!event.target.matches('.navbar-toggler')) {
       var dropdowns = document.getElementsByClassName("dropdown-content");
@@ -15,3 +13,26 @@ function myFunction() {
       }
     }
   }
+
+const slide = document.querySelector('.carousel-slide');
+const prevBtn = document.querySelector('.carousel-prev');
+const nextBtn = document.querySelector('.carousel-next');
+const slideWidth = slide.clientWidth;
+
+let currentPosition = 0;
+
+prevBtn.addEventListener('click', () => {
+  currentPosition += slideWidth;
+  if (currentPosition >= slideWidth) {
+    currentPosition = -2 * slideWidth;
+  }
+  slide.style.transform = `translateX(${currentPosition}px)`;
+});
+
+nextBtn.addEventListener('click', () => {
+  currentPosition -= slideWidth;
+  if (currentPosition <= -3 * slideWidth) {
+    currentPosition = 0;
+  }
+  slide.style.transform = `translateX(${currentPosition}px)`;
+});
